6e53c5f09660c56c9a99bb85d44f0cd382e43b90
ShogoInoue <haneogohs1002@gmail.com>
2017-09-28 03:47:55 +0000

    login画面きれいにした

--- travel/travel_app/db/schema.rb
+++ travel/travel_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926065308) do
+ActiveRecord::Schema.define(version: 20170927072328) do
 
   create_table "plan_tags", force: :cascade do |t|
     t.integer  "plan_id"
