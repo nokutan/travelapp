2d08b4ffa311e43d72c05dd116737b528835da3c
tomonaka <rtmakerobots@gmail.com>
2017-09-28 05:31:20 +0000

    change plans_contoroller

--- travel/travel_app/app/controllers/plans_controller.rb
+++ travel/travel_app/app/controllers/plans_controller.rb
@@ -2,33 +2,17 @@ class PlansController < ApplicationController
     before_action :setup_plan_tag,only:[:add_tag,:delete_tag]
     def show
        @plan=Plan.all
-       
-       @plan.count.times do |i|
-         @plan_tags=@plan[i].tags
-       end
+       #@plan_tag = plan_structure.tags
     end
     
-    
-    #タグの一覧からプランに追加をする時のアクション
-    def add_tag
-        if @plan_tag.blank?
-            @plan_tag=plan_structure.plan.build(tag_id: params[:tag_id])
-        end
-        @plan_tag.save
-        redirect_to plan_structure
+    def add_plan
+    end
+
+    def edit_plan
     end
     
-    #def update_tag?
-    
-    #プランの画面からタグを消すときのアクション
-    def delete_tag
-        @plan_tag.destroy
-        redirect_to plan_structure
+    def delete_plan
     end
     
-    #bef_act
-    def setup_plan_tag!
-        @plan_tag=plan_structure.plan_tags.find_by(tag_id: params[tag_id])
-    end
     
 end
