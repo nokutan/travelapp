eae9842fef28306a6c8d48b039817fdb7c026597
KeijiroKanda <uorijiekadnak82606991@gmail.com>
2017-09-28 04:11:26 +0000

    The page shows that the plan has some tags.

--- travel/travel_app/app/controllers/dynamic_pages_controller.rb
+++ travel/travel_app/app/controllers/dynamic_pages_controller.rb
@@ -1,5 +1,31 @@
 class DynamicPagesController < ApplicationController
-  def home
-      @tags = Tag.all.order(created_at: 'desc')
-  end
+    before_action :setup_plan_tag,only:[:add_tag,:delete_tag]
+
+    def home
+        @plan=Plan.first
+        @plan_tags=Plan.first.tags
+    end
+    
+    #タグの一覧からプランに追加をする時のアクション
+    def add_tag
+        if @plan_tag.blank?
+            @plan_tag=plan_structure.plan.build(tag_id: params[:tag_id])
+        end
+        @plan_tag.save
+        redirect_to plan_structure
+    end
+    
+    #def update_tag?
+    
+    #プランの画面からタグを消すときのアクション
+    def delete_tag
+        @plan_tag.destroy
+        redirect_to plan_structure
+    end
+    
+    #bef_act
+    def setup_plan_tag!
+        @plan_tag=plan_structure.plan_tags.find_by(tag_id: params[tag_id])
+    end
+    
 end
